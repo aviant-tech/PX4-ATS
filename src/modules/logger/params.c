54f6632544b7a3393e05a5b0b0fadc2a1c56dd65
Torjus Bakkene <torjus.bakkene@gmail.com>
2026-03-17 15:09:42 +0100

    logger: only log to SD card by default

--- src/modules/logger/params.c
+++ src/modules/logger/params.c
@@ -87,7 +87,7 @@ PARAM_DEFINE_INT32(SDLOG_MODE, 0);
  * @reboot_required true
  * @group SD Logging
  */
-PARAM_DEFINE_INT32(SDLOG_BACKEND, 3);
+PARAM_DEFINE_INT32(SDLOG_BACKEND, 1);
 
 /**
  * Battery-only Logging
